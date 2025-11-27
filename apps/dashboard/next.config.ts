e3f7fe07012c667149c27d5baf69e0d78be782c9
Alain <alain00.alvarez@gmail.com>
2025-11-27 15:59:56 -0500

    chore: upgrade next

--- apps/dashboard/next.config.ts
+++ apps/dashboard/next.config.ts
@@ -2,35 +2,32 @@ import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: false,
-  poweredByHeader: false,
-  reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  transpilePackages: ["@mimir/integration", "@mimir/api", "@mimir/ui"],
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-      {
-        protocol: "http",
-        hostname: "(localhost|127.0.0.1)",
-      },
-    ],
-  },
+	devIndicators: false,
+	poweredByHeader: false,
+	reactStrictMode: true,
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	transpilePackages: ["@mimir/integration", "@mimir/api", "@mimir/ui"],
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "**",
+			},
+			{
+				protocol: "http",
+				hostname: "(localhost|127.0.0.1)",
+			},
+		],
+	},
 };
 
 export default withSentryConfig(nextConfig, {
-  org: process.env.SENTRY_ORG,
-  project: process.env.SENTRY_PROJECT,
-  silent: !process.env.CI,
-  widenClientFileUpload: true,
-  disableLogger: true,
-  automaticVercelMonitors: true,
+	org: process.env.SENTRY_ORG,
+	project: process.env.SENTRY_PROJECT,
+	silent: !process.env.CI,
+	widenClientFileUpload: true,
+	disableLogger: true,
+	automaticVercelMonitors: true,
 });
